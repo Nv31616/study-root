78be837cea83b5de6cf27edb990e8005b4b09cd6
Nv31616 <nishantv10092005@gmail.com>
2026-06-08 13:42:43 +0530

    Moved images to public folder for deployment

--- components/Computers.jsx
+++ components/Computers.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Computers = () => {
   return (
     <img 
-      src="../src/assets/Computers.png" 
+      src="../public/Computers.png" 
       className="h-3/4 place-content-center self-center my-[4.8px]"
     />
   );
